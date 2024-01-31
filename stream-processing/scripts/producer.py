77092f8d4ceca4cfd205ab01c730476b661298ff
Tarifi Hicham <tarifi.hicham@gmail.com>
2024-01-31 11:28:15 +0100

    add reviews topic to get movie reviews details

--- stream-processing/scripts/producer.py
+++ stream-processing/scripts/producer.py
@@ -3,13 +3,15 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
-# from kafka import KafkaProducer
+from kafka import KafkaProducer
 
 
 # Kafka server address
 kafka_host = "localhost:9092"  
+
 # Topic name
-topic_name = "moviedb"
+movie_topic = "moviedb"
+review_topic = "reviewdb"
 
 # Load environment variables from .env
 load_dotenv()
@@ -20,35 +22,54 @@ API_KEY = os.getenv('API_KEY')
 # Page number of the endpoint
 page = 1
 
-
 # Create a Kafka producer
 producer = KafkaProducer(bootstrap_servers=kafka_host)
 
 print("Fetshing start.")
 
 while True:
+    MAX_RETRIES = 3
+    WAIT_TIME = 5
     print("********** PAGE ***********", page)
-    # Call the API to fetch data
-    MOVIE_ENDPOINT = "https://api.themoviedb.org/3/trending/person/day?api_key={}&language=en-US&page={}"
-    response = requests.get(MOVIE_ENDPOINT.format(API_KEY, page))
+    for retry in range(MAX_RETRIES):
+        try:
+            # Call the API to fetch data
+            MOVIE_ENDPOINT = "https://api.themoviedb.org/3/trending/movie/day?api_key={}&language=en-US&page={}"
+            response = requests.get(MOVIE_ENDPOINT.format(API_KEY, page))
 
-    if response.status_code == 200:
-        actors = response.json()["results"]
-        if actors:
-            for actor in actors:
-                # Check if "known_for" field exists and is not empty
-                if "known_for" in actor and len(actor["known_for"]) > 0:
-                    # Produce the actor to Kafka
-                    producer.send(topic_name, value=json.dumps(actor).encode('utf-8'))
-                    # print("Send actors data to spark. Wait 5 seconds...")
-                    time.sleep(5)
+            if response.status_code == 200:
+                movies = response.json()["results"]
+                if movies:
+                    for movie in movies:
+                        # Check if "id" field exists and is not empty
+                        print(f"{str(movie)} \n")
+                        if "id" in movie and movie["id"] is not None:
+                            REVIEW_ENDPOINT = "https://api.themoviedb.org/3/movie/{}/reviews?api_key={}&language=en-US&page=1"
+                            response = requests.get(REVIEW_ENDPOINT.format(int(movie["id"]), API_KEY))
+                            producer.send(movie_topic, value=json.dumps(movie).encode('utf-8'))
+                            if response.status_code == 200:
+                                reviews = response.json()["results"]
+                                # Produce the movie to Kafka
+                                for review in reviews:
+                                    REVIEW_ENDPOINT = "https://api.themoviedb.org/3/review/{}?api_key={}"
+                                    response = requests.get(REVIEW_ENDPOINT.format(review["id"], API_KEY))
+                                    if response.status_code == 200:
+                                        review_detail = response.json()
+                                        producer.send(review_topic, value=json.dumps(review_detail).encode('utf-8'))
+                                        print(f"{str(review_detail)} \n")
+                                print("\n##########################################\n")
+                                # print("Send movies data to spark. Wait 5 seconds...")
+                                time.sleep(5)
+                        else:
+                            print("Empty or missing 'id' field. Skipping sending to spark.")
                 else:
-                    print("Empty or missing 'known_for' field. Skipping sending to spark.")
-        else:
-            print("Empty data. Skipping sending to spark.")
-    else:
-        print("Error fetching data from TMDb API")
-
+                    print("Empty data. Skipping sending to spark.")
+            else:
+                print("Error fetching data from TMDb API")
+        except requests.exceptions.RequestException as e:
+            print("An error occurred while requesting reviews:", e)
+        print("Retrying in", WAIT_TIME, "seconds...")
+        time.sleep(WAIT_TIME)
     page = page + 1
 
 print("Fetshing done.")
